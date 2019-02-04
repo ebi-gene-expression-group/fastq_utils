5500018f6efb14d8bd15b5c9fd22acea986f17c5
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-04 09:15:03 +0000

    Strip back deps install

--- install_deps.sh
+++ install_deps.sh
@@ -5,13 +5,13 @@ mkdir -p deps
 
 ############################################
 ## zlib
-wget -c http://zlib.net/zlib-1.2.11.tar.gz
-tar xvzf zlib-1.2.11.tar.gz
-pushd zlib-1.2.11
-./configure --prefix `pwd`/..
-make
-make install
-popd
+#wget -c http://zlib.net/zlib-1.2.11.tar.gz
+#tar xvzf zlib-1.2.11.tar.gz
+#pushd zlib-1.2.11
+#./configure --prefix `pwd`/..
+#make
+#make install
+#popd
 #############################################
 #
 if [ ! -e deps/samtools-0.1.19.tar.bz2  ]; then
@@ -25,14 +25,14 @@ popd
 
 ###############################################
 # Downloads were failing often so keep a local copy to speedup the installation
-echo "Installing samtools 1.x" 
-if [ ! -e deps/samtools-1.9.tar.bz2 ] ; then
-    wget -c https://sourceforge.net/projects/samtools/files/samtools/1.9/samtools-1.9.tar.bz2/download -O deps/samtools-1.9.tar.bz2
-fi
+#echo "Installing samtools 1.x" 
+#if [ ! -e deps/samtools-1.9.tar.bz2 ] ; then
+#    wget -c https://sourceforge.net/projects/samtools/files/samtools/1.9/samtools-1.9.tar.bz2/download -O deps/samtools-1.9.tar.bz2
+#fi
 
-tar xvjf deps/samtools-1.9.tar.bz2
-pushd samtools-1.9
-./configure  prefix=$TOPLEVEL_DIR
-make -j $J
+#tar xvjf deps/samtools-1.9.tar.bz2
+#pushd samtools-1.9
+#./configure  prefix=$TOPLEVEL_DIR
+#make -j $J
 #prefix=$TOPLEVEL_DIR
-make install prefix:=$TOPLEVEL_DIR
+#make install prefix:=$TOPLEVEL_DIR
